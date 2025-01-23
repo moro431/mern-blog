ab6757569abbd4b1c1086971f79d54df0aee7012
moro <moro02823@gmail.com>
2025-01-23 16:50:42 +0100

    last commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,25 @@
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Home from './pages/Home';
-// import About from './pages/About';
-// import SignIn from './pages/SignIn';
-// import Dashboard from './pages/Dashboard';
-// import Projects from './pages/Projects';
-// import SignUp from './pages/SignUp';
-// import Header from './components/Header';
-// import Footer from './components/Footer';
-// import PrivateRoute from './components/PrivateRoute';
-// import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
-// import CreatePost from './pages/CreatePost';
-// import UpdatePost from './pages/UpdatePost';
-// import PostPage from './pages/PostPage';
-// import ScrollToTop from './components/ScrollToTop';
-// import Search from './pages/Search';
+ import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Projects from './pages/Projects';
+import Daschboard from './pages/Daschboard';
 
 export default function App() {
   return (
-    <div>
-      <h1 className="text-3xl text-red-500">hello</h1>
-    </div>
-    // <BrowserRouter>
-    //   <ScrollToTop />
-    //   <Header />
-    //   <Routes>
-    //     <Route path='/' element={<Home />} />
-    //     <Route path='/about' element={<About />} />
-    //     <Route path='/sign-in' element={<SignIn />} />
-    //     <Route path='/sign-up' element={<SignUp />} />
-    //     <Route path='/search' element={<Search />} />
-    //     <Route element={<PrivateRoute />}>
-    //       <Route path='/dashboard' element={<Dashboard />} />
-    //     </Route>
-    //     <Route element={<OnlyAdminPrivateRoute />}>
-    //       <Route path='/create-post' element={<CreatePost />} />
-    //       <Route path='/update-post/:postId' element={<UpdatePost />} />
-    //     </Route>
-
-    //     <Route path='/projects' element={<Projects />} />
-    //     <Route path='/post/:postSlug' element={<PostPage />} />
-    //   </Routes>
-    //   <Footer />
-    // </BrowserRouter>
+    // <div>
+    //   <h1 className="text-3xl text-red-500">hello</h1>
+    // </div>
+    <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+           <Route path='/dashboard' element={<Daschboard />} />
+           <Route path='/projects' element={<Projects/>} />
+      </Routes>
+     </BrowserRouter>
   );
 }
---
+++ client/src/pages/Daschboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Daschboard() {
+  return (
+    <div>
+      <h1>Daschboard</h1>
+    </div>
+  )
+}

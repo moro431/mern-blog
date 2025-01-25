c24afbe3188acc138a17dde329f3994df1096f30
moro <moro02823@gmail.com>
2025-01-25 07:03:36 +0100

    add functionality to the sign up page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
  import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import SignIn from './pages/SignIn';
+import SignIn from './pages/SingIn';
 import SignUp from './pages/SignUp';
 import Projects from './pages/Projects';
 import Daschboard from './pages/Daschboard';

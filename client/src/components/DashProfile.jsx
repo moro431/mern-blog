97e9653724525f7db651738e695e4abf2eb11116
moro <moro02823@gmail.com>
2025-01-28 16:42:07 +0100

    complete profil update page functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,6 +1,11 @@
-import { Button, TextInput } from "flowbite-react";
+import { Alert, Button, TextInput } from "flowbite-react";
 import { useEffect, useRef, useState } from "react";
-
+import {
+  updateStart,
+  updateSuccess,
+  updateFailure,
+} from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
 import { useSelector } from "react-redux";
 
 
@@ -9,6 +14,11 @@ export default function DashProfile() {
     const [imageFile,setImageFile] = useState(null);
     const [imageFileUrl,setImageFileUrl] = useState(null);
     const filePickerRef = useRef();
+    const [formData, setFormData] = useState({});
+    const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+    const [updateUserError, setUpdateUserError] = useState(null);
+    const dispatch = useDispatch();
+
     const handleImageChange = (e) => {
         const file = e.target.files[0];
         if(file){
@@ -28,10 +38,49 @@ export default function DashProfile() {
         console.log('upload image ....');
         
     }
+
+    const handleChange = (e) => {
+      setFormData({ ...formData, [e.target.id]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      setUpdateUserError(null);
+      setUpdateUserSuccess(null);
+      if (Object.keys(formData).length === 0) {
+        setUpdateUserError('No changes made');
+        return;
+      }
+      // if (imageFileUploading) {
+      //   setUpdateUserError('Please wait for image to upload');
+      //   return;
+      // }
+      try {
+        dispatch(updateStart());
+        const res = await fetch(`/api/user/update/${currentUser._id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          dispatch(updateFailure(data.message));
+          setUpdateUserError(data.message);
+        } else {
+          dispatch(updateSuccess(data));
+          setUpdateUserSuccess("User's profile updated successfully");
+        }
+      } catch (error) {
+        dispatch(updateFailure(error.message));
+        setUpdateUserError(error.message);
+      }
+    };
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
     <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
-    <form  className='flex flex-col gap-4'>
+    <form  className='flex flex-col gap-4'  onSubmit={handleSubmit}>
       <input type="file" accept="image/*" onChange={handleImageChange} ref={filePickerRef} hidden/>
       <div
         className='relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'
@@ -50,20 +99,20 @@ export default function DashProfile() {
         id='username'
         placeholder='username'
         defaultValue={currentUser.username}
-        // onChange={handleChange}
+        onChange={handleChange}
       />
       <TextInput
         type='email'
         id='email'
         placeholder='email'
         defaultValue={currentUser.email}
-        // onChange={handleChange}
+        onChange={handleChange}
       />
       <TextInput
         type='password'
         id='password'
         placeholder='password'
-        // onChange={handleChange}
+        onChange={handleChange}
       />
       <Button
         type='submit'
@@ -82,6 +131,16 @@ export default function DashProfile() {
           Sign Out
         </span>
       </div>
+      {updateUserSuccess && (
+        <Alert color='success' className='mt-5'>
+          {updateUserSuccess}
+        </Alert>
+      )}
+        {updateUserError && (
+        <Alert color='failure' className='mt-5'>
+          {updateUserError}
+        </Alert>
+      )}
    </div>
 );
   
